a9d18d474f1b1b38f6673c889c7f8c11e2cd774b
LucasCorpT5 <lucas.contatoestud@gmail.com>
2023-05-17 17:21:35 -0400

    paybleController_upd3

--- src/controllers/payableController.ts
+++ src/controllers/payableController.ts
@@ -1,7 +1,9 @@
 import { Transaction } from ".prisma/client";
+import { payableService } from "../services/payableService";
+import { Request, Response } from "express";
 
 export const payableController = {
-    createPayable: async(transaction: Transaction) => {
+    createPayable: async(req: Request, res: Response, transaction: Transaction) => {
         const payableDate = new Date();
         const payableAmount = transaction.amount * 0.95; // desconto de 5% para a taxa do PSP
         const payableStatus = transaction.paymentMethod === 'debit_card' ? 'paid' : 'waiting_funds';
@@ -10,5 +12,13 @@ export const payableController = {
         if(transaction.paymentMethod === "credit_card") {
             payablePaymentDate.setDate(payableDate.getDate() + 30);
         }
-    }        
+
+        const payableCreated = payableService.createPayable(payableDate, payableAmount, payableStatus, transaction);
+    
+        return res.json(payableCreated);
+    },
+    
+    getCustomerBalance: async(req: Request, res: Response) => {
+        
+    }
 }
--- src/services/payableService.ts
+++ src/services/payableService.ts
@@ -13,5 +13,30 @@ export const payableService = {
               paidAt: payableStatus === 'paid' ? payableDate : null,
             }
         });
+
+        return payable;
+    },
+
+    getCustomerBalance: async(cardNumber: string) => {
+        const payables = await prisma.payable.findMany({
+            where: {
+                transaction: {
+                    cardNumber,
+                }
+            }
+        });
+
+        const availableBalance = payables
+            .filter(payable => payable.status === "paid")
+            .reduce((total, payable) => total + payable.amount, 0);
+
+        const waitingFundsBalance = payables
+            .filter(payable => payable.status === "waiting_funds")
+            .reduce((total, payable) => total + payable.amount, 0);
+
+        return {
+            availableBalance,
+            waitingFundsBalance
+        };
     }
 }
