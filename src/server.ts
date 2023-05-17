e6856f97b1631325494665a4972bc288d1c59844
LucasCorpT5 <lucas.contatoestud@gmail.com>
2023-05-17 15:29:50 -0400

    first_commit

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log("Servidor rodando na porta " + PORT);
+});
